9c146ad466346d07ca5aa023cf548a09e1d6bb56
Elgadisalma <elgadi.salma0@gmail.com>
2024-03-26 15:01:40 +0000

    CRUD permissions

--- oauth2/app/Http/Controllers/permissionController.php
+++ oauth2/app/Http/Controllers/permissionController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Permission;
 
 class permissionController extends Controller
 {
-    //
+    public function addPermission(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $permission = new Permission;
+        $permission->name = $request->name;
+        $permission->save();
+
+        return response()->json(['message' => 'permission added successfully', 'permission' => $permission], 201);
+    }
+
+    public function deletePermission($id)
+    {
+        $permission = Permission::find($id);
+        $permission->delete();
+
+        if (!$permission) {
+            return response()->json(['message' => 'permission not found'], 404);
+        }
+
+        return response()->json(['message' => 'permission deleted successfully'], 200);
+    }
+
+    public function showPermissions()
+    {
+        $permissions = Permission::all();
+        return response()->json(['message' => 'Liste des permissions', 'permissions' => $permissions], 200);
+    }
 }
--- oauth2/routes/api.php
+++ oauth2/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 use App\Http\Controllers\roleController;
+use App\Http\Controllers\permissionController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -27,3 +28,13 @@ Route::post('/users', [userController::class, 'addUser']);
 Route::get('/roles', [RoleController::class, 'showRoles']);
 Route::post('/roles', [roleController::class, 'addRole']);
 Route::delete('/roles/{id}', [roleController::class, 'deleteRole']);
+
+
+/*
+|--------------------------------------------------------------------------
+| CRUD permissions
+|--------------------------------------------------------------------------
+*/
+Route::get('/permissions', [permissionController::class, 'showPermissions']);
+Route::post('/permissions', [permissionController::class, 'addPermission']);
+Route::delete('/permissions/{id}', [permissionController::class, 'deletePermission']);
