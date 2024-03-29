6906fc6ff0f26fd7b8bc115d63e3eb7daa77ad4a
Elgadisalma <elgadi.salma0@gmail.com>
2024-03-29 11:34:43 +0000

    Update auth.php

--- oauth2/config/auth.php
+++ oauth2/config/auth.php
@@ -40,14 +40,13 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-    
         'api' => [
-            'driver' => 'token',
+            'driver' => 'passport',
             'provider' => 'users',
         ],
     ],
 
-    
+
     /*
     |--------------------------------------------------------------------------
     | User Providers
