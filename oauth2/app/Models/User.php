6f57c9d3a0433874b3a1f01f3412c947acc861ce
Elgadisalma <elgadi.salma0@gmail.com>
2024-03-29 11:36:39 +0000

    Update User.php

--- oauth2/app/Models/User.php
+++ oauth2/app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Role;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 
 
 class User extends Authenticatable
@@ -47,5 +48,9 @@ class User extends Authenticatable
     public function roles()
     {
         return $this->belongsToMany(Role::class);
+    }
+    public function hasRole($role)
+    {
+        return $this->roles()->where('name', $role)->exists();
     }
 }
